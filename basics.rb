67715369d1abd3c9833576b15c2f319fba185c0b
Jordan Corley <rainwolfj@gmail.com>
2016-03-07 14:04:22 -0600

    Difficult mode completed

--- basics.rb
+++ basics.rb
@@ -1,75 +1,58 @@
-print "1. "
-puts 4 == 2 * 2
+def question(prompt, body)
+  print prompt
+  puts body
+end
 
-print "2. "
-puts 5 > 4
+question("1. ", 4 == 2 * 2)
 
-print "3. "
-puts (3 * 4) > 10
+question("2. ", 5 > 4)
 
-print "4. "
-puts 3 == 3
+question("3. ", (3 * 4) > 10)
 
-print "5. "
-puts (1 > 0) && (1 <= 1) == true
+question("4. ", 3 == 3)
 
-print "6. "
-puts 4 != 2
+question("5. ", (1 > 0) && (1 <= 1) == true)
 
-print "7. "
-puts (4 <= 10) || (2 >= 3) == true
+question("6. ", 4 != 2)
 
-print "8. "
-puts 9 % 3 == 0
+question("7. ", (4 <= 10) || (2 >= 3) == true)
 
-print "9. "
-puts 100 == 10 ** 2
+question("8. ", 9 % 3 == 0)
 
-print "10. "
-puts 6 == 1000 / (300 / 2)
+question("9. ", 100 == 10 ** 2)
 
+question("10. ", 6 == 1000 / (300 / 2))
 #I AM A COMMENT RUBY IGNORES ME BECAUSE MY LINE STARTED WITH A '#'
 #Use the following variable msg for questions 11 - 15
 msg = "hEllO"
 
-print "11. "
-puts msg.upcase == "HELLO"
+question("11. ", msg.upcase == "HELLO")
 
-print "12. "
-puts msg.swapcase == "HeLLo"
+question("12. ", msg.swapcase == "HeLLo")
 
-print "13. "
-puts msg.reverse == "OllEh"
+question("13. ", msg.reverse == "OllEh")
 
-print "13a. "
-puts msg.upcase.reverse == "OLLEH"
+question("13a. ", msg.upcase.reverse == "OLLEH")
 
 arr = ['a', 'b', 'c']
 
-print "14. "
-puts arr.last == 'c'
 
-print "15. "
-puts 3 == arr.count
+question("14. ", arr.last == 'c')
 
-print "16. "
-puts arr*"," == 'a,b,c'
+question("15. ", 3 == arr.count)
 
+question("16. ", arr*"," == 'a,b,c')
 #Hint: This will take 2 method calls
 print "17. "
 puts arr.last.upcase == 'C'
-
+question("17. ", 3 == arr.count)
 hsh = { name: 'Fido', age: '99' }
 print "18. "
 puts  hsh.fetch(:name) == 'Fido'
+question("18. ", 3 == arr.count)
 
-print "19. "
-puts hsh.keys == [:name, :age]
+question("19. ", 3 == arr.count)
 
-print "20. "
-puts hsh.values == ['Fido', '99']
+question("20. ", 3 == arr.count)
 
-
-print "21. "
-hsh.store(:special, 'Dog')
-puts hsh == { name: 'Fido', age: '99', special: 'Dog' }
+question("21. ", (hsh.store(:special, 'Dog'); hsh == { name: 'Fido', age: '99', special: 'Dog' }))
