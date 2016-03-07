ea0cb337d5bbe3b6ad9d9489aeb8ce2719dd5085
Jordan Corley <rainwolfj@gmail.com>
2016-03-07 15:23:52 -0600

    I think I'm epic, things appear to work, corrected final questions being incorrect

--- basics.rb
+++ basics.rb
@@ -1,58 +1,34 @@
 def question(prompt, body)
   print prompt
+  print ". "
   puts body
 end
 
-question("1. ", 4 == 2 * 2)
+def start
 
-question("2. ", 5 > 4)
+  msg = "hEllO"
+  arr = ['a', 'b', 'c']
+  hsh = { name: 'Fido', age: '99' }
 
-question("3. ", (3 * 4) > 10)
-
-question("4. ", 3 == 3)
-
-question("5. ", (1 > 0) && (1 <= 1) == true)
-
-question("6. ", 4 != 2)
-
-question("7. ", (4 <= 10) || (2 >= 3) == true)
-
-question("8. ", 9 % 3 == 0)
-
-question("9. ", 100 == 10 ** 2)
-
-question("10. ", 6 == 1000 / (300 / 2))
-#I AM A COMMENT RUBY IGNORES ME BECAUSE MY LINE STARTED WITH A '#'
-#Use the following variable msg for questions 11 - 15
-msg = "hEllO"
-
-question("11. ", msg.upcase == "HELLO")
-
-question("12. ", msg.swapcase == "HeLLo")
-
-question("13. ", msg.reverse == "OllEh")
-
-question("13a. ", msg.upcase.reverse == "OLLEH")
-
-arr = ['a', 'b', 'c']
+  increment = 1
+  questions = [0,(4 == 2 * 2),(5 > 4),((3 * 4) > 10), (3 == 3), ((1 > 0) && (1 <= 1) == true),(4 != 2), ((4 <= 10) || (2 >= 3) == true),
+  (9 % 3 == 0), (100 == 10 ** 2), (6 == 1000 / (300 / 2)), (msg.upcase == "HELLO"), (msg.swapcase == "HeLLo"), (msg.reverse == "OllEh"),
+  (arr.last == 'c'),(3 == arr.count),(arr*"," == 'a,b,c'),(arr.last.upcase == 'C'),(hsh.fetch(:name) == 'Fido'),(hsh.keys == [:name, :age]),
+  (hsh.values == ['Fido', '99']),((hsh.store(:special, 'Dog'); hsh == { name: 'Fido', age: '99', special: 'Dog' }))]
 
 
-question("14. ", arr.last == 'c')
+  21.times do
 
-question("15. ", 3 == arr.count)
+  question(increment.to_s, questions[increment])
 
-question("16. ", arr*"," == 'a,b,c')
-#Hint: This will take 2 method calls
-print "17. "
-puts arr.last.upcase == 'C'
-question("17. ", 3 == arr.count)
-hsh = { name: 'Fido', age: '99' }
-print "18. "
-puts  hsh.fetch(:name) == 'Fido'
-question("18. ", 3 == arr.count)
+  if (increment == 13)
+    question("13a", msg.upcase.reverse == "OLLEH")
+  end
+  increment +=1
 
-question("19. ", 3 == arr.count)
+  end
+end
+questions = [0,(4 == 2 * 2)]
+increment = 1
 
-question("20. ", 3 == arr.count)
-
-question("21. ", (hsh.store(:special, 'Dog'); hsh == { name: 'Fido', age: '99', special: 'Dog' }))
+start
